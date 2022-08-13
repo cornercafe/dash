c3578622759e19e35a48d891f87fb4e8587a5fc8
amjed-ali-k <amjedmgm@gmail.com>
2022-08-13 19:41:20 +0530

    Added Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,73 @@
 import './App.css';
 
+import { Icon } from '@iconify/react';
 import React, { useState } from 'react';
 
-import logo from './logo.svg';
-
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [mode, setMode] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="header">
-          🚀 Vite + React + Typescript 🤘 & <br />
-          Eslint 🔥+ Prettier
-        </p>
-
-        <div className="body">
-          <button onClick={() => setCount((count) => count + 1)}>
-            🪂 Click me : {count}
-          </button>
-
-          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
+    <div className="flex h-screen">
+      <div className="w-96 text-white bg-black h-full">
+        <div>
+          <p className="text-9xl font-bold text-center">71°</p>
         </div>
-      </header>
+        <div>
+          <ul className="px-9">
+            <li className="flex justify-center">
+              <RoundButton
+                active={mode === 0}
+                onClick={() => setMode(0)}
+                icon="bxs:leaf"
+                label="Eco"
+              />
+              <RoundButton
+                active={mode === 1}
+                onClick={() => setMode(1)}
+                icon="fluent:glance-default-12-regular"
+                label="Normal"
+              />
+              <RoundButton
+                active={mode === 2}
+                onClick={() => setMode(2)}
+                icon="carbon:meter-alt"
+                label="Max"
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const RoundButton = ({
+  icon,
+  label,
+  active = false,
+  onClick,
+}: {
+  icon: string;
+  label: string;
+  active?: boolean;
+  onClick?: () => void;
+}) => (
+  <div className="flex m-2 flex-col items-center">
+    <button
+      onClick={onClick}
+      className={`rounded-full hover:opacity-90 duration-200 p-3 ${
+        active ? 'bg-purple-200 text-gray-800' : 'bg-zinc-700 text-white'
+      }`}
+    >
+      <Icon className="w-6 h-6" icon={icon} />
+    </button>
+    <p
+      className={`pt-1 font-medium text-sm my-0 ${
+        active ? 'text-white' : 'text-zinc-700'
+      }`}
+    >
+      {label}
+    </p>
+  </div>
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,48 @@
 import './index.css';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createTheme, NextUIProvider, Theme } from '@nextui-org/react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { useDarkMode } from 'usehooks-ts';
 
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const fonts = {
+  sans: `"Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+  'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;"`,
+};
+
+const sharedTheme: Theme = {
+  theme: {
+    fonts,
+  },
+};
+
+export const lightTheme = createTheme({
+  ...sharedTheme,
+  type: 'light',
+});
+
+export const darkTheme = createTheme({
+  ...sharedTheme,
+  type: 'dark',
+});
+
+const MyApp = () => {
+  const { isDarkMode } = useDarkMode(false);
+  return (
+    <NextUIProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <App />
+    </NextUIProvider>
+  );
+};
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <StrictMode>
+      <MyApp />
+    </StrictMode>,
+  );
+}
